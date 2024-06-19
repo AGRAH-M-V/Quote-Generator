e72135451643bde022d460ce343efa31f66697db
agrah m v <agrahmv@gmail.com>
2024-06-19 22:49:15 +0530

    first commit

---
+++ src/Quote.jsx
@@ -0,0 +1,49 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import './index.css'; 
+
+const Quote = () => {
+    const [quote, setQuote] = useState('');
+    const [author, setAuthor] = useState('');
+    const [isFetching, setIsFetching] = useState(false);
+
+    const fetchQuote = async () => {
+        setIsFetching(true);
+        try {
+            const response = await axios.get('https://api.quotable.io/random');
+            setQuote(response.data.content);
+            setAuthor(response.data.author);
+        } catch (error) {
+            console.error("Error fetching the quote", error);
+        } finally {
+            setIsFetching(false);
+        }
+    };
+
+    useEffect(() => {
+        fetchQuote();
+    }, []);
+
+    return (
+        <div className="quoteContainer">
+            {isFetching ? (
+                <div className="spinner"></div>
+            ) : (
+                <div className="quote">
+                    <h2 className="title">Quote</h2>
+                    <p className="text">"{quote}"</p>
+                    <p className="author">- {author}</p>
+                    <button
+                        className={isFetching ? 'newQuoteButtonDisabled' : 'newQuoteButton'}
+                        onClick={fetchQuote}
+                        disabled={isFetching}
+                    >
+                        {isFetching ? 'Fetching...' : 'New Quote'}
+                    </button>
+                </div>
+            )}
+        </div>
+    );
+};
+
+export default Quote;
